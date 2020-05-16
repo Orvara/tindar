ec8a0b121030aa9d483c141ceaa31d8a65ab8eb9
Orvara <Orvara@users.noreply.github.com>
2020-05-16 20:51:24 +0000

    Update settings.py

--- tindar/settings.py
+++ tindar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-joha%8x2paod)&80t)7ltqh!wm&s$@l$vwb1ec!e^=^^446s+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [140.82.112.3]
+ALLOWED_HOSTS = [orvar.pythonanywhere.com]
 
 
 # Application definition
